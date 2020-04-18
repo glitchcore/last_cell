a9f7fa4c7b908ebd09e36b11a87d202db3d4969e
glitchcore <mail@s3f.ru>
2020-04-19 01:22:14 +0600

    wip life game view

--- Main.gd
+++ Main.gd
@@ -7,16 +7,97 @@ extends Spatial
 
 var line
 var node_2d
+var cells_state = []
 
 const X_SIZE = 20
 const Y_SIZE = 20
 
+var viewport_size = Vector2(0, 0)
+
+func draw_cell(cell, x, y):
+	var points = PoolVector2Array()
+	
+	points.push_back(Vector2(
+		viewport_size.x * float(x)/X_SIZE,
+		viewport_size.y * float(y)/Y_SIZE
+	))
+	points.push_back(Vector2(
+		viewport_size.x * float(x + 1)/X_SIZE,
+		viewport_size.y * float(y)/Y_SIZE
+	))
+	points.push_back(Vector2(
+		viewport_size.x * float(x + 1)/X_SIZE,
+		viewport_size.y * float(y + 1)/Y_SIZE
+	))
+	points.push_back(Vector2(
+		viewport_size.x * float(x)/X_SIZE,
+		viewport_size.y * float(y + 1)/Y_SIZE
+	))
+	
+	var color = PoolColorArray([Color("FFFFFF")])
+	
+	node_2d.draw_polygon(points, color)
+
+func get_neighbours(state, x, y):
+	# get Fon-neumann 1 rank w circular
+	return [
+		state[x + 1 if x + 1 < X_SIZE else 0][y],
+		state[x - 1 if x > 0 else X_SIZE - 1][y],
+		
+		state[x][y + 1 if y + 1 < Y_SIZE else 0],
+		state[x][y - 1 if y > 0 else Y_SIZE - 1],
+		
+		state[x + 1 if x + 1 < X_SIZE else 0][y + 1 if y + 1 < Y_SIZE else 0],
+		state[x + 1 if x + 1 < X_SIZE else 0][y - 1 if y > 0 else Y_SIZE - 1],
+		
+		state[x - 1 if x > 0 else X_SIZE - 1][y + 1 if y + 1 < Y_SIZE else 0],
+		state[x - 1 if x > 0 else X_SIZE - 1][y - 1 if y > 0 else Y_SIZE - 1]
+	]
+
+enum {FN_CONWAYS_LIFE}
+
+func conways_life(current_cell, neighbours):
+	var live_neighbours = []
+	for neighbour in neighbours:
+		if neighbour.state:
+			live_neighbours.append(neighbour)
+	
+	var neighbours_count = len(live_neighbours)
+	
+	var new_state = false
+	
+	if not current_cell.state and neighbours_count == 3 :
+		new_state = true
+	
+	if current_cell.state and (neighbours_count == 2 or neighbours_count == 3):
+		new_state = true
+	
+	return {
+		"cell_fn": FN_CONWAYS_LIFE,
+		"state": new_state
+	}
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var viewport_size = get_node("Viewport").size
+	viewport_size = get_node("Viewport").size
 	
 	line = get_node("Viewport/Node2D/Line2D")
 	node_2d = get_node("Viewport/Node2D")
+	
+	for x in range(X_SIZE):
+		var col = []
+		for y in range(Y_SIZE):
+			# init cells state
+			var cell = {
+				"cell_fn": FN_CONWAYS_LIFE,
+				"state": false
+			}
+			
+			col.append(cell)
+			
+			draw_cell(cell, x, y)
+		
+		cells_state.append(col)
 	
 	# draw grid
 	for x in range(X_SIZE):
@@ -35,7 +116,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var position = line.get_point_position(0)
-	position.x += 10 * delta
-	position.y += 10 * delta
-	line.set_point_position(0, position)
+	pass
+	var new_cells_state = []
+	
+	for x in range(X_SIZE):
+		var col = []
+		for y in range(Y_SIZE):
+			# init cells state
+			var current_cell = cells_state[x][y]
+			
+			var new_cell = null
+			
+			match current_cell.cell_fn:
+				FN_CONWAYS_LIFE:
+					new_cell = conways_life(
+						current_cell,
+						get_neighbours(cells_state, x, y)
+					)
+				_:
+					new_cell = null
+			
+			col.append(new_cell)
+			
+			if not new_cell == current_cell:
+				draw_cell(new_cell, x, y)
+		
+		new_cells_state.append(col)
