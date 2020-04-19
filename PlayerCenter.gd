0d3a4a7eb4a853b24b36aed9fd64452b57fdefd2
Irina Molodykh <jairinka@gmail.com>
2020-04-20 01:29:41 +0600

    cube -> sphere

--- PlayerCenter.gd
+++ PlayerCenter.gd
@@ -31,17 +31,17 @@ func _process(_delta):
 func _input(event):
 	if event is InputEventMouseMotion:
 		pass
-#		camera.rotation.x = clamp(
-#			camera.rotation.x - event.relative.y * MOUSE_SPEED,
-#			deg2rad(-80), deg2rad(0)
-#		)
-		
-		# rotation.y -= event.relative.x * MOUSE_SPEED
-	
+##		camera.rotation.x = clamp(
+##			camera.rotation.x - event.relative.y * MOUSE_SPEED,
+##			deg2rad(-80), deg2rad(0)
+##		)
 #
-#	if Input.is_action_pressed("ui_right"):
-#		direction.x += 1
-#	if Input.is_action_pressed("ui_up"):
-#		direction.z -= 1
-#	if Input.is_action_pressed("ui_down"):
-#		direction.z += 1
+#		# rotation.y -= event.relative.x * MOUSE_SPEED
+#
+##
+##	if Input.is_action_pressed("ui_right"):
+##		direction.x += 1
+##	if Input.is_action_pressed("ui_up"):
+##		direction.z -= 1
+##	if Input.is_action_pressed("ui_down"):
+##		direction.z += 1
